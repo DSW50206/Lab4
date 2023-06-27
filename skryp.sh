851e21375040a991b1e70e41620f4f07109e31f7
DSW50206 <dsw50206@student.dsw.edu.pl>
2023-06-27 20:27:48 +0200

    Dodano trzeci mylnik

--- skryp.sh
+++ skryp.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+if [[ "$1" == "--logs" ]]; then
+    for ((i=1; i<=100; i++))
+    do
+        echo "Nazwa pliku: log$i.txt" >> log$i.txt
+        echo "Skrypt: skrypt.sh" >> log$i.txt
+        echo "Data: $(date)" >> log$i.txt
+    done
+    echo "Utworzono 100 plików logx.txt"
+    exit 0
+fi
 echo "Dzisiejsza data: $(date)"
