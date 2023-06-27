a3518c22c1381204ccb329968b340e09a942a696
DSW50206 <dsw50206@student.dsw.edu.pl>
2023-06-27 20:49:49 +0200

    Dodano czwarty mylnik

--- skryp.sh
+++ skryp.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 if [[ "$1" == "--logs" ]]; then
-    for ((i=1; i<=100; i++))
+	if [[ "$2" =~ ^[0-9]+$ ]]; then
+        	licznik=$2
+    	else
+        	licznik=100
+    	fi
+
+    for ((i=1; i<=$licznik; i++))
     do
         echo "Nazwa pliku: log$i.txt" >> log$i.txt
         echo "Skrypt: skrypt.sh" >> log$i.txt
         echo "Data: $(date)" >> log$i.txt
     done
-    echo "Utworzono 100 plików logx.txt"
+    echo "Utworzono $licznik plików logx.txt"
     exit 0
 fi
 echo "Dzisiejsza data: $(date)"
