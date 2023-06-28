00eafc84aa4d60d3bc255ae7ad68ded1eedbd6fb
DSW50206 <dsw50206@student.dsw.edu.pl>
2023-06-28 18:13:04 +0200

    Dodano czwarty myślnik częsci drugiej

--- skryp.sh
+++ skryp.sh
@@ -21,6 +21,26 @@ if [[ "$1" == "--date" ]] || [[ "$1" == "-d" ]]; then
     exit 0
 fi
 
+if [[ "$1" == "--error" ]] || [[ "$1" == "-e" ]]; then
+    if [[ "$2" =~ ^[0-9]+$ ]]; then
+        licznik=$2
+    else
+        licznik=100
+    fi
+
+    for ((i=1; i<=$licznik; i++))
+    do
+        echo "Nazwa pliku: error$i.txt" >> error$i.txt
+        echo "Skrypt: skrypt.sh" >> error$i.txt
+        echo "Data: $(date)" >> error$i.txt
+
+    done
+
+    echo "Utworzono $licznik plików errorx.txt"
+    exit 0
+fi
+
+
 if [[ "$1" == "--help" ]] || [[ "$1" == "-h" ]]; then
     echo "Dostępne opcje:"
     echo "--logs - Tworzy 100 plików logx.txt"
